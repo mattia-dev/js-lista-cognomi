10097f66ad45ebe9b5f602a664e74da552d0dd8f
mattia-dev <82512569+mattia-dev@users.noreply.github.com>
2021-06-03 16:15:29 +0200

    ex. done

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
 var output = document.getElementById('output');
+
+var surnames = ["Bianchi", "Neri", "Rossi", "Verdi", "Gialli"];
+
+var userSurname = prompt("Qual è il tuo cognome?");
+
+surnames.push(userSurname);
+
+surnames.sort();
+
+var i = 0;
+
+while (i < surnames.length) {
+    output.innerHTML += surnames[i] + "</br>";
+    i++;
+}
+
+var position = surnames.indexOf(userSurname) + 1;
+
+output.innerHTML += "Il tuo congome si trova in posizione " + position + " in ordine alfabetico.";
